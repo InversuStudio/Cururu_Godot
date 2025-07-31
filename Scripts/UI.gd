5fd2848a8329cd1a26b826e59b671d54dc63303a
Sutabu <162359580+Sutabu@users.noreply.github.com>
2025-07-30 22:47:06 -0300

    Fix LOAD
    Consertado problema com código que crasha jogo ao tentar ler arquivo não existente

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,9 +1,11 @@
 extends Control
 
 func _ready() -> void:
-	GameData.moedas = GameData.config.get_value("save", "moedas")
-	GameData.update_moeda.connect(UpdateMoeda)
-	%CounterMoeda.text = str(GameData.moedas)
+	var file = OS.get_executable_path().get_base_dir()+"/savedata.cfg"
+	if FileAccess.file_exists(file):
+		GameData.moedas = GameData.config.get_value("save", "moedas")
+		GameData.update_moeda.connect(UpdateMoeda)
+		%CounterMoeda.text = str(GameData.moedas)
 
 func UpdateMoeda() -> void:
 	%CounterMoeda.text = str(GameData.moedas)
