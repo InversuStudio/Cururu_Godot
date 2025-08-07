9bb528e1e56effede5217d0baefc962146ee7780
Inversu Game Studio <games@inversustudio.com.br>
2025-08-07 10:51:50 -0300

    Visual Design
    Alteração e implementação do design de inimigo

--- Scripts/MenuPrincipal.gd
+++ Scripts/MenuPrincipal.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_main_pressed() -> void:
-	Mundos.CarregaFase(Mundos.NomeFase.Mata_Atlantica_MA_01)
+	Mundos.CarregaFase(Mundos.NomeFase.Mata_Atlantica_MA_02)
 
 func _on_fase_teste_pressed() -> void:
 	if GameData.Load():
