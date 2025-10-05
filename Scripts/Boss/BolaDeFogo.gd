d224f250619d886d3594655d79b86cb27ad34bd9
Inversu Game Studio <games@inversustudio.com.br>
2025-10-05 00:52:15 -0300

    Ajustes boss

--- Scripts/Boss/BolaDeFogo.gd
+++ Scripts/Boss/BolaDeFogo.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var speed:Vector2 = Vector2(1,0)
 
 func _process(delta: float) -> void:
-	global_position += speed * 128 * delta
+	global_position += speed * 170 * delta
 
 func _on_hit_box_hit() -> void:
 	Console._Print("PAPAPAPAPAPAP")
