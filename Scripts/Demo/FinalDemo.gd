c9cb1f1205959a8394ec8608cc9c41cde6cd1cc0
Inversu Game Studio <games@inversustudio.com.br>
2025-10-18 22:58:33 -0300

    AJustes

--- Scripts/Demo/FinalDemo.gd
+++ Scripts/Demo/FinalDemo.gd
@@ -1,6 +1,6 @@
 extends Control
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
+	if event.is_action_pressed("ui_accept"): # Enter / Espaço / Botão A do joystick etc
 		Mundos.CarregaFase(Mundos.NomeFase.MenuPrincipal)
 	#if Input.is_action_just_pressed("melee")
