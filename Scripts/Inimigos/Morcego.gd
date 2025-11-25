99d6b54e0b76688fd8e2ad4505fd5e9b7f89ecba
Theo42FM <theo.magalhaes42@outlook.com>
2025-11-25 16:11:29 -0300

    Morcego.gb Atualizado
    Alguns componentes renomeados para melhor entendimento. Movimento e caça ao player consertados, não estavam funcionando.

--- Scripts/Inimigos/Morcego.gd
+++ Scripts/Inimigos/Morcego.gd
@@ -3,12 +3,10 @@ extends CharacterBody2D
 #Só movimento feito, ainda é necessário rever outras ideias
 # Podexá, cumpadi -G
 
-@export var speed: float = 2
+@export var speed: float = 200
 var dir: Vector2 = Vector2.ZERO # Sempre adiciono um valor padrão, porque computadores.
-
 var _hunting: bool = false
-
-var trava_move:bool = false
+var trava_move: bool = false
 
 @export_group("Pushback")
 @export var distancia_push:float = 1.0
@@ -18,25 +16,21 @@ func _ready() -> void:
 	%HurtBox.hurt.connect(RecebeuDano)
 	%HitBox.hit.connect(Pushback)
 
-func _process(delta) -> void:
+func _physics_process(delta: float) -> void:
 	if !trava_move:
-		move(delta)
+		if _hunting:
+			velocity = global_position.direction_to(Mundos.player.global_position) * speed
+		else:
+			velocity = dir * speed
+	#Flip da sprite
+	%Sprite.flip_h = false if velocity.x < 0 else true
 	move_and_slide()
 
-func move(delta) -> void:
-	if _hunting:
-		velocity = global_position.direction_to(Mundos.player.global_position) * speed * 128
-	elif !_hunting:
-		velocity += dir * speed * delta
-	
-	#Flipagem mais bonita existente
-	# Sempre fico feliz quando uso -G
-	%Sprite.flip_h = false if velocity.x < 0 else true
-
 func _on_timer_timeout():
-	$Timer.wait_time = choose([1.0, 1.5, 2.0])
+	$Timer.wait_time = randf_range(0.5, 1.5)
 	if !_hunting:
-		dir = choose([Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN])
+		dir = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+		#dir = choose([Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN])
 		print(dir)
 
 func choose(array):
@@ -46,12 +40,12 @@ func choose(array):
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		_hunting = true
-		print("caçando")
+		#print("caçando")
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		_hunting = false
-		print("todeboah")
+		#print("idle")
 
 func RecebeuDano(_h:Array[HitBox]) -> void:
 	Console._Print("MORCEGO AIAIAI")
