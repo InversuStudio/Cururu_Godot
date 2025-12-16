26b32a4c067c63760edc352c92640060e431206d
Sutabu <162359580+Sutabu@users.noreply.github.com>
2025-12-16 20:22:01 -0300

    Boss Quick Fix
    Pequenos ajustes no boss

--- Scripts/Boss/Pilar.gd
+++ Scripts/Boss/Pilar.gd
@@ -5,16 +5,19 @@ extends Node2D
 ## Tempo de efeito do pilar
 @export var tempo_ativo:float = 2.0
 # Recebe se pilar está ativo
-var ativo:bool = false
+#var ativo:bool = false
 
 func _ready() -> void:
 	$HitBox/CollisionShape2D.disabled = true
 	$Timer.start(tempo_carga)
+	$Sprite.connect("animation_finished", func(): queue_free())
 
 func _on_timer_timeout() -> void:
-	if ativo: queue_free()
-	else:
-		ativo = true
-		$Sprite.play("Jato")
-		$Timer.start(tempo_ativo)
-		$HitBox/CollisionShape2D.disabled = false
+	$Sprite.play("Jato")
+	$HitBox/CollisionShape2D.disabled = false
+	#if ativo: queue_free()
+	#else:
+		#ativo = true
+		#$Sprite.play("Jato")
+		#$Timer.start(tempo_ativo)
+		#$HitBox/CollisionShape2D.disabled = false
