58e8c276481493e52541689556bbcc186712d9ad
Inversu Game Studio <games@inversustudio.com.br>
2026-01-13 10:24:06 -0300

    VFX
    - Adicionado o VFX de Entrar e Sair da Água
    - Vaso quebrando
    - Sprite de Vasos Diversos

--- Scripts/Globais/AvisoItem.gd
+++ Scripts/Globais/AvisoItem.gd
@@ -6,11 +6,13 @@ signal tela_item
 
 func _ready() -> void:
 	%AvisoItem.modulate.a = 0.0
+	%ColorRect.visible = false
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept") and ativo:
 		%Anim.play("TelaOff")
 		await %Anim.animation_finished
+		%ColorRect.visible = false
 		ativo = false
 		get_tree().paused = false
 		tela_item.emit()
@@ -21,5 +23,6 @@ func Mostra(nome:String, desc:String, img:Texture2D) -> void:
 	%DescItemAviso.text = desc
 	%ImgItemAviso.texture = img
 	%Anim.play("TelaOn")
+	%ColorRect.visible = true
 	await %Anim.animation_finished
 	ativo = true
