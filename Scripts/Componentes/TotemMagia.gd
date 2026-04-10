edbbbb1dcdaebf2838fbe204811fccf398fcf9b4
Sutabu <162359580+Sutabu@users.noreply.github.com>
2026-04-10 18:54:42 -0300

    %
    %

--- Scripts/Componentes/TotemMagia.gd
+++ Scripts/Componentes/TotemMagia.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 ## Porcentagem da magia total a ser recuperada
-@export_range(1, 100, 1) var magia_a_recuperar:float = 50
+@export_range(1, 100, 1, "suffix:%") var magia_a_recuperar:float = 50
 
 func Morte() -> void:
 	var result:int = roundi((magia_a_recuperar / 100) * GameData.magia_max)
