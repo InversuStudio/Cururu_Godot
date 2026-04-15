4dfec2056a0efa76f73e37c573e44dd7e3ee1c69
W0odz <pedrovmadeira@gmail.com>
2026-04-15 20:07:07 -0300

    Terceira tentativa de ajuste do melee

--- Objetos/Funcionalidade/vfx_melee.gd
+++ Objetos/Funcionalidade/vfx_melee.gd
@@ -4,6 +4,10 @@ func _ready() -> void:
 	var c:AnimatedSprite2D = $Animation
 	var hit_col:CollisionPolygon2D = $HitBox/CollisionPolygon2D
 	hit_col.set_deferred("disabled", true)
-	c.frame_changed.connect(func():
-		if c.frame >= 1:
-			hit_col.set_deferred("disabled", false))
+	
+	# Calcula metade da duração do primeiro frame
+	var frame_duration:float = (1.0 / c.sprite_frames.get_animation_speed(c.animation)) / c.speed_scale
+	var meio_frame:float = frame_duration * 0.5  # ← ajusta esse multiplicador (0.5 = meio frame)
+	
+	await get_tree().create_timer(meio_frame).timeout
+	hit_col.set_deferred("disabled", false)
